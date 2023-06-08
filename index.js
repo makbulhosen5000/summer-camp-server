c2c4bb2edb0f45f165967ee927d2c01efd0f8d9e
mh akash <mhakash5000@gmail.com>
2023-06-08 11:04:46 +0600

    added database with mongodb and load data from mongodb

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT||5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ahuevic.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+      const summerCollection = client.db("summerCamp").collection('popularClasses');
+    
+    app.get('/popular-classes',async(req,res)=>{
+      const result = await summerCollection.find().toArray();
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Summer Camp App Server is Running')
+})
+
+app.listen(port, () => {
+  console.log(`Summer Camp app server on port ${port}`)
+})
