02e40d6d2a5825e4938f0d6b19914863279d5dd5
mh akash <mhakash5000@gmail.com>
2023-06-12 00:42:26 +0600

    delted cart by id

--- index.js
+++ index.js
@@ -69,6 +69,14 @@ async function run() {
       res.send(result);
     })
 
+    //for delete cart one by one
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
